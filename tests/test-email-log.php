08aac46ad5011d25945002e691659f2d881340ef
Sudar Muthu <sudar@sudarmuthu.com>
2016-07-02 12:58:27 +0530

    Setup the test class for testing EmailLog

--- tests/test-email-log.php
+++ tests/test-email-log.php
@@ -1,26 +1,45 @@
 <?php
-/**
- * Class SampleTest
- *
- * @package Email_Log
- */
-
-require_once( dirname( dirname( __FILE__ ) ) . '/email-log.php' );
 
 /**
- * Sample test case.
+ * Test main plugin class.
  */
 class Email_Log_Tests extends WP_UnitTestCase {
+	protected $object;
 
-	/**
-	 * A single example test.
-	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+	public function setUp() {
+		parent::setUp();
+		$this->object = new EmailLog();
 	}
 
-	function test_add_plugin_links() {
-		$links = add_plugin_links($links, $file);
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	/**
+	 * Test include_path.
+	 */
+	public function test_include_path() {
+		// Plugin Folder Path
+		$expected = str_replace( 'tests/', '', plugin_dir_path( __FILE__ ) );
+		$actual   = $this->object->include_path;
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Test translations.
+	 * @TODO: Daniel
+	 */
+	public function test_translations() {
+
+	}
+
+	/**
+	 * Test included files.
+	 */
+	public function test_included_files() {
+		$this->assertFileExists( $this->object->include_path . 'include/install.php' );
+
+		//@TODO: Daniel add it for other included files
 	}
 }
