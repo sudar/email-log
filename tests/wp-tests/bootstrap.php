55f0a1fecc54c2a5a3a7cd4b5f7fe2927892cd97
Sudar Muthu <sudar@sudarmuthu.com>
2016-07-26 11:44:43 +0530

    Configure .travis to run uninstall tests

--- tests/wp-tests/bootstrap.php
+++ tests/wp-tests/bootstrap.php
@@ -7,18 +7,27 @@ if ( ! $_tests_dir ) {
 
 require_once $_tests_dir . '/includes/functions.php';
 
-// Include the uninstall test tools functions.
-include_once dirname( __FILE__ ) . '/../../vendor/jdgrimes/wp-plugin-uninstall-tester/includes/functions.php';
+$_uninstall_test_lib_exists = false;
+if ( file_exists( dirname( __FILE__ ) . '/../../vendor/jdgrimes/wp-plugin-uninstall-tester/' ) ) {
+	$_uninstall_test_lib_exists = true;
+}
+
+if ( $_uninstall_test_lib_exists ) {
+	// Include the uninstall test tools functions.
+	include_once dirname( __FILE__ ) . '/../../vendor/jdgrimes/wp-plugin-uninstall-tester/includes/functions.php';
+}
 
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/../email-log.php';
 }
 
 // Check if the tests are running. Only load the plugin if they aren't.
-if ( ! running_wp_plugin_uninstall_tests() ) {
+if ( ! $_uninstall_test_lib_exists || ! running_wp_plugin_uninstall_tests() ) {
 	tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 }
 
 require $_tests_dir . '/includes/bootstrap.php';
 
-include_once dirname( __FILE__ ) . '/../../vendor/jdgrimes/wp-plugin-uninstall-tester/bootstrap.php';
+if ( $_uninstall_test_lib_exists ) {
+	include_once dirname( __FILE__ ) . '/../../vendor/jdgrimes/wp-plugin-uninstall-tester/bootstrap.php';
+}
