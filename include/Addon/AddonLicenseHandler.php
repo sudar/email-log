40bf9ef8a056f69bb4a526ad1b7b712236b163fb
Sudar Muthu <sudar@sudarmuthu.com>
2017-01-14 09:12:30 +0530

    Add Add-on related classes

---
+++ include/Addon/AddonLicenseHandler.php
@@ -0,0 +1,91 @@
+<?php namespace EmailLog\Addon;
+
+use EmailLog\Core\UI\Component\AddonLicenseRenderer;
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+/**
+ * Addon License Handler.
+ *
+ * @since 2.0.0
+ */
+class AddonLicenseHandler {
+
+	const STORE_URL = 'https://wpemaillog.com';
+
+	private $addon_file;
+	private $addon_name;
+	private $addon_version;
+	private $addon_author;
+
+	/**
+	 * Instance of EDD Plugin Updater.
+	 *
+	 * @var \EDD_SL_Plugin_Updater
+	 */
+	private $updater;
+
+	/**
+	 * Addon License field Renderer.
+	 *
+	 * @var EmailLog\UI\Addon\AddonLicenseRenderer
+	 */
+	private $renderer;
+
+	/**
+	 * AddonLicenseHandler constructor.
+	 *
+	 * @param string                                 $addon_file Add-on main file.
+	 * @param EmailLog\UI\Addon\AddonLicenseRenderer $renderer   Addon License Renderer.
+	 */
+	public function __construct( $addon_file, $renderer ) {
+		$this->addon_file = $addon_file;
+		$this->renderer = $renderer;
+	}
+
+	/**
+	 * Set Add-on data.
+	 *
+	 * @param string $addon_name    Add-on Name.
+	 * @param string $addon_version Add-on Version.
+	 * @param string $addon_author  Add-on Author.
+	 */
+	public function set_addon_data( $addon_name, $addon_version, $addon_author ) {
+		$this->addon_name    = $addon_name;
+		$this->addon_version = $addon_version;
+		$this->addon_author  = $addon_author;
+
+		$this->renderer->set_addon_data( $addon_name, $addon_version );
+	}
+
+	/**
+	 * Set up hooks and load the license handler.
+	 * This method is called on `wp-loaded` hook.
+	 */
+	public function load() {
+		add_action( 'admin_init', array( $this, 'setup_updater' ) );
+
+		$this->renderer->load();
+	}
+
+	/**
+	 * Setup up Add-on auto-updater user EDD library.
+	 */
+	public function setup_updater() {
+		// TODO: Hook the license key.
+		$license_key = '';
+
+		if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) ) {
+			$email_log = email_log();
+			require_once $email_log->get_plugin_path() . 'include/libraries/EDD_SL_Plugin_Updater.php';
+		}
+
+		$this->updater = new \EDD_SL_Plugin_Updater( self::STORE_URL, $this->addon_file, array(
+				'version'   => $this->addon_version,
+				'license'   => $license_key,
+				'item_name' => $this->addon_name,
+				'author'    => $this->addon_author,
+			)
+		);
+	}
+}
---
+++ include/Addon/EmailLogAddon.php
@@ -0,0 +1,52 @@
+<?php namespace EmailLog\Addon;
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+/**
+ * Base Email Log Addon.
+ *
+ * @since 2.0.0
+ */
+abstract class EmailLogAddon {
+
+	protected $addon_file;
+	protected $addon_name = '';
+	protected $addon_version = '';
+	protected $addon_author = 'Sudar Muthu';
+
+	/**
+	 * Addon License Handler.
+	 *
+	 * @var EmailLog\Addon\AddonLicenseHandler.
+	 */
+	private $license_handler;
+
+	/**
+	 * Initialize add-on data.
+	 *
+	 * @access protected
+	 */
+	abstract protected function initialize();
+
+	/**
+	 * Construct a new EmailLogAddon instance.
+	 *
+	 * @param string                             $addon_file      Addon main file.
+	 * @param EmailLog\Addon\AddonLicenseHandler $license_handler Addon License Handler.
+	 */
+	public function __construct( $addon_file, $license_handler ) {
+		$this->addon_file      = $addon_file;
+		$this->license_handler = $license_handler;
+
+		$this->initialize();
+	}
+
+	/**
+	 * Load the add-on and setup hooks.
+	 * This method is called on `wp-loaded` hook.
+	 */
+	public function load() {
+		$this->license_handler->set_addon_data( $this->addon_name, $this->addon_version, $this->addon_author );
+		$this->license_handler->load();
+	}
+}
