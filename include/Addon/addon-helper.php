350a602a249e37feaaf91b805d9650bb76884da5
Sudar Muthu <sudar@sudarmuthu.com>
2017-01-14 09:14:20 +0530

    Modify add-on helper function to include license related code

--- include/Addon/addon-helper.php
+++ include/Addon/addon-helper.php
@@ -9,18 +9,21 @@
 /**
  * Load an Email Log add-on.
  *
- * @since 2.0
+ * @since 2.0.0
  *
  * @param string $addon_class Add-on class name.
- * @param string $plugin_file Plugin File.
+ * @param string $addon_file  Add-on File.
  */
-function load_email_log_addon( $addon_class, $plugin_file ) {
+function load_email_log_addon( $addon_class, $addon_file ) {
 	$email_log = email_log();
 
-	$plugin_dir = plugin_dir_path( $plugin_file );
-	$email_log->loader->add_namespace( 'EmailLog', $plugin_dir . 'include' );
+	$addon_dir = plugin_dir_path( $addon_file );
+	$email_log->loader->add_namespace( 'EmailLog', $addon_dir . 'include' );
 
-	$addon = new $addon_class( $plugin_file );
+	$license_renderer = new EmailLog\Core\UI\Addon\AddonLicenseRenderer();
+	$license_handler = new EmailLog\Addon\AddonLicenseHandler( $addon_file, $license_renderer );
+
+	$addon = new $addon_class( $addon_file, $license_handler );
 
 	add_action( 'el_loaded', array( $addon, 'load' ) );
 }
