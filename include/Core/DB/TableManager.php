3ec4c79f60e53d64ba87ce28227d4602d5547861
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2017-02-11 02:27:49 +0530

    Add a method to delete logs beyond given interval in TableManager class

--- include/Core/DB/TableManager.php
+++ include/Core/DB/TableManager.php
@@ -239,4 +239,19 @@ class TableManager {
 		$query      = $wpdb->prepare( 'SELECT * FROM ' . $table_name . ' WHERE id = %d', $id );
 		return $wpdb->get_results( $query );
 	}
+
+    /**
+     * Deletes Email Logs older than the specified interval.
+     *
+     * @param $interval_in_days
+     * @return int
+     */
+	public function delete_logs_older_than( $interval_in_days ) {
+        global $wpdb;
+        $table_name = $this->get_log_table_name();
+
+        $query      = $wpdb->prepare( 'DELETE FROM ' . $table_name . ' WHERE sent_date < DATE_SUB( CURDATE(), INTERVAL %d DAY )', $interval_in_days );
+
+        return $wpdb->query( $query );
+    }
 }
