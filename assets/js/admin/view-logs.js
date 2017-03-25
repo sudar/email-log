649c0643327cf9c06095e28e76c364cde699a205
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2017-03-25 13:09:12 +0530

    Add HTML/Text tabs to Thickbox

--- assets/js/admin/view-logs.js
+++ assets/js/admin/view-logs.js
@@ -1,11 +1,22 @@
 ( function( $ ) {
 
-    $( document ).ready(function() {
-        $( '#search_id-search-date-input' ).datepicker({
-            changeMonth: true,
-            changeYear: true,
-            dateFormat: 'yy-mm-dd'
-        });
-    });
+	$( document ).ready(function() {
+		$( '#search_id-search-date-input' ).datepicker({
+			changeMonth: true,
+			changeYear: true,
+			dateFormat: 'yy-mm-dd'
+		});
+	});
+
+	var tabsInsertedEvent = 'tabs_elem_inserted';
+
+	insertionQ( '#tabs' ).every(function ( element ) {
+		$( element ).trigger( tabsInsertedEvent )
+	});
+
+	$( document ).on( tabsInsertedEvent, function() {
+		$( '#tabs' ).tabs();
+	});
+
 
 })( jQuery );
