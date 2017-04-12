f0e533ffa4ccf299152e38759ce4c2328c0ca4c7
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-04-12 04:13:44 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- include/Core/Request/LogListAction.php
+++ include/Core/Request/LogListAction.php
@@ -156,7 +156,7 @@ class LogListAction implements Loadie {
 	/**
 	 * Get TableManager instance.
 	 *
-	 * @return TableManager TableManager instance.
+	 * @return \EmailLog\Core\DB\TableManager TableManager instance.
 	 */
 	protected function get_table_manager() {
 		$email_log = email_log();
