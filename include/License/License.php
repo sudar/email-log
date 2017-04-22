51e7ac9cf0529df9999f4359c6d92f36787bca40
Sudar Muthu <sudar@sudarmuthu.com>
2017-04-22 17:50:06 +0530

    Remove unused get_download_url method

--- include/License/License.php
+++ include/License/License.php
@@ -27,19 +27,4 @@ class License extends BaseLicense {
 	public function get_license_key() {
 		return $this->license_key;
 	}
-
-	/**
-	 * Get the download url for the add-on.
-	 *
-	 * @return string Download url.
-	 */
-	public function get_download_url() {
-		try {
-			$response = $this->get_version();
-		} catch ( \Exception $e ) {
-			return '';
-		}
-
-		return $response->download_link;
-	}
 }
