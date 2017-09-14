a03f016940039354c88cc611c81aaf0305099dd7
Sudar Muthu <sudar@sudarmuthu.com>
2017-09-14 06:04:52 +0530

    Run PHPCompatiblity code sniffs in Travis

---
+++ bin/phpcs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Run phpcs with PHPCompatibility sniffs.
+
+vendor/bin/phpcs -p email-log.php --standard=PHPCompatibility --runtime-set testVersion 5.2
+vendor/bin/phpcs -p *.php --standard=PHPCompatibility --runtime-set testVersion 5.3-7.2
+vendor/bin/phpcs -p include/ --standard=PHPCompatibility --runtime-set testVersion 5.3-7.2
