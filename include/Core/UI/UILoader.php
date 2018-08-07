67e2b54a74e3b1b4dfed529e8803613a263f3e54
Rajan <rajanit2000@gmail.com>
2018-08-07 19:35:07 +0000

    Apply fixes from StyleCI

--- include/Core/UI/UILoader.php
+++ include/Core/UI/UILoader.php
@@ -2,7 +2,6 @@
 
 use EmailLog\Core\Loadie;
 use EmailLog\Core\UI\Page\LogListPage;
-use EmailLog\Core\UI\Page\SystemInfoPage;
 
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
@@ -81,9 +80,9 @@ class UILoader implements Loadie {
 	 * @access protected
 	 */
 	protected function initialize_pages() {
-		$this->pages['log_list_page']   = new Page\LogListPage();
-		$this->pages['settings_page']   = new Page\SettingsPage();
-		$this->pages['addon_list_page'] = new Page\AddonListPage();
+		$this->pages['log_list_page']    = new Page\LogListPage();
+		$this->pages['settings_page']    = new Page\SettingsPage();
+		$this->pages['addon_list_page']  = new Page\AddonListPage();
 		$this->pages['system_info_page'] = new Page\SystemInfoPage();
 	}
 }
