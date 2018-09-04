600d5684015f08b841d62a30b5acbecfa7c72bc3
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-09-04 18:48:17 +0530

    Show admin notice regardless of the notfication email sent status

--- include/Core/UI/Setting/CoreSetting.php
+++ include/Core/UI/Setting/CoreSetting.php
@@ -370,11 +370,17 @@ class CoreSetting extends Setting {
 		$logs_threshold_met      = $db_size_notification_data['log_threshold_met'];
 
 		// Ideally threshold cannot be 0. Also, skip sending email if it is already sent.
-		if ( ! $is_notification_enabled || $logs_threshold === 0 || $logs_threshold_met === true ) {
+		if ( $logs_threshold === 0 || $logs_threshold_met === true ) {
 			return;
 		}
 
-		if ( is_email( $admin_email ) && ( $logs_count >= $logs_threshold ) ) {
+		if ( $logs_count < $logs_threshold ) {
+			return;
+		}
+
+		$this->register_threshold_met_admin_notice();
+
+		if ( $is_notification_enabled && is_email( $admin_email ) ) {
 			$subject = sprintf( __( 'Email Log Plugin: Your log threshold of %s has been met', 'email-log' ),
 				$logs_threshold );
 			$message = <<<EOT
@@ -407,8 +413,6 @@ EOT;
 
 			$setting_data[ $db_size_notification_key ]['log_threshold_met'] = true;
 			\update_option( $this->section->option_name, $setting_data );
-
-			$this->register_threshold_met_admin_notice();
 		}
 	}
 
