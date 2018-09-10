bee10e99ee12dbb79af379ea6a310b696ae2f303
esudalaimadan <ezhil.madan@gmail.com>
2018-09-10 12:02:50 +0530

    Group info and add email log config info
    Fix #157

--- include/Core/UI/Component/EmailLogSystemInfo.php
+++ include/Core/UI/Component/EmailLogSystemInfo.php
@@ -36,12 +36,12 @@ class EmailLogSystemInfo extends SystemInfo {
 Email Log Version:                  <?php echo $this->get_plugin_version() . "\n"; ?>
 Number of Logs:                     <?php echo $email_log->table_manager->get_logs_count() . "\n"; ?>
 Email Log DB Version:               <?php echo get_option( TableManager::DB_OPTION_NAME ) . "\n"; ?>
-<?php if ( $this->get_license_key === '' ) : ?>
+<?php if ( $this->get_license_key() === '' ) : ?>
 License Key:                        <?php echo $this->get_license_key() . "\n"; ?>
 <?php endif; ?>
 <?php $email_log_core = get_option('email-log-core'); ?>
 <?php if ( $email_log_core ) : ?>
-Allowed Users for Email Log View:   <?php echo implode( ', ', $email_log_core['allowed_user_roles'] ) . "\n"; ?>
+Allowed Roles for Email Log View:   <?php echo implode( ', ', $email_log_core['allowed_user_roles'] ) . "\n"; ?>
 Remove All Data on Uninstallation:  <?php echo $email_log_core['remove_on_uninstall'] !== '' ? "Yes" : "No" . "\n"; ?>
 Disable Dash Board Widget:          <?php echo $email_log_core['hide_dashboard_widget'] === 'true' ? "Yes" : "No" . "\n"; ?>
 <?php endif; ?>
