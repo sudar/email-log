433990b10d2799011c56139219c9dd6b94dbd967
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2019-01-15 17:12:35 +0530

    Add additional column labels

--- include/Util/helper.php
+++ include/Util/helper.php
@@ -261,9 +261,12 @@ function get_advanced_search_url() {
  */
 function get_column_label_by_db_column( $db_column ) {
 	$labels = array(
+		'id'        => __( 'ID', 'email-log' ),
 		'sent_date' => __( 'Sent at', 'email-log' ),
-		'to'        => __( 'To', 'email-log' ),
+		'to'        => __( 'To', 'email-log' ), // EmailLog\Core\UI\ListTable::get_columns() uses `to`
+		'to_email'  => __( 'To', 'email-log' ),
 		'subject'   => __( 'Subject', 'email-log' ),
+		'message'   => __( 'Message', 'email-log' ),
 	);
 
 	// TODO: @sudar, Do we need a filter here to add items to the $labels array?
