sudar/email-log

commit 6f180ea3fc497057ad861942968bd7d382f684e8

Replace deprecated wp get sites() with get sites()
Fix #66

include/Core/DB/TableManager.php
@@ -41,8 +41,7 @@ class TableManager implements Loadie {
 		if ( is_multisite() && $network_wide ) {
 			// Note: if there are more than 10,000 blogs or
 			// if `wp_is_large_network` filter is set, then this may fail.
-			// TODO: Take care of the deprecated function.
-			$sites = wp_get_sites();
+			$sites = get_sites();
 
 			foreach ( $sites as $site ) {
 				switch_to_blog( $site['blog_id'] );

uninstall.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 if ( is_multisite() ) {
 	// Note: if there are more than 10,000 blogs or
 	// if `wp_is_large_network` filter is set, then this may fail.
-	$sites = wp_get_sites();
+	$sites = get_sites();
 
 	foreach ( $sites as $site ) {
 		switch_to_blog( $site['blog_id'] );

commit 7ff71274f8399e3852c7385c90173f4781de1523

get_sites function returns WP_Site object and not array
Fix the issue that was introduced by 6f180ea3fc497057ad861942968bd7d382f684e8

Fix #66

include/Core/DB/TableManager.php
@@ -48,7 +48,7 @@ class TableManager implements Loadie {
 			$sites = get_sites();
 
 			foreach ( $sites as $site ) {
-				switch_to_blog( $site['blog_id'] );
+				switch_to_blog( $site->blog_id );
 				$this->create_table_if_needed();
 				restore_current_blog();
 			}

uninstall.php
@@ -16,7 +16,7 @@ if ( is_multisite() ) {
 	$sites = get_sites();
 
 	foreach ( $sites as $site ) {
-		switch_to_blog( $site['blog_id'] );
+		switch_to_blog( $site->blog_id );
 		email_log_delete_db_data();
 		restore_current_blog();
 	}