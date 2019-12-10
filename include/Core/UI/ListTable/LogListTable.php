1258fe7b1b588fbebea6aa1275e26651961c5454
Sudar Muthu <sudar@sudarmuthu.com>
2019-12-10 07:32:24 +0000

    Apply fixes from StyleCI

--- include/Core/UI/ListTable/LogListTable.php
+++ include/Core/UI/ListTable/LogListTable.php
@@ -63,7 +63,6 @@ class LogListTable extends \WP_List_Table {
 	 *
 	 * @since 2.3.0 Retrieve Column labels using Utility methods.
 	 * @since 2.3.2 Added `result` column.
-	 *
 	 * @see WP_List_Table::single_row_columns()
 	 *
 	 * @uses \EmailLog\Util\get_column_label()
